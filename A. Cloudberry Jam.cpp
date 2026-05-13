829c4d7f9e36429f66b4e2089cbaeee76366ecef
AizuruPRJCT <mirzadarmawan80@gmail.com>
2026-05-13 14:17:52 +0700

    Add files via upload

---
+++ A. Cloudberry Jam.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t;
+   if (!(cin >> t)) return 0;
+
+  while(t--){
+    long long n;
+    cin >> n;   
+
+    long long res=2*n;
+
+    cout<<res<<endl;
+  }
+
+
+}
